a915fd543de7fe18bfcd327f6ed61da3f8bccfbe
Matías MI <lumen.night@gmail.com>
2014-05-26 12:55:01 -0300

    Moved sources to specific directories to clean up codebase.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
-  InMemoryStorer: require("./lib/InMemoryStorer"),
-  FileSystemStorer: require("./lib/FileSystemStorer"),
-  FileSystemCache: require("./lib/FileSystemCache"),
-  BillImporter: require("./lib/BillImporter"),
-  CommitteeImporter: require("./lib/CommitteeImporter")
+  InMemoryStorer: require("./lib/bill/InMemoryStorer"),
+  FileSystemStorer: require("./lib/bill/FileSystemStorer"),
+  FileSystemCache: require("./lib/bill/FileSystemCache"),
+  BillImporter: require("./lib/bill/BillImporter"),
+  CommitteeImporter: require("./lib/committee/CommitteeImporter")
 };
